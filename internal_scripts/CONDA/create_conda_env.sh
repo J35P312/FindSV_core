f23bc5b6decfd3091c8c288e6c0e77b80348e470
Jesper Eisfeldt <J35P312@users.noreply.github.com>
2016-06-07 22:09:02 +0200

    Update create_conda_env.sh

--- internal_scripts/CONDA/create_conda_env.sh
+++ internal_scripts/CONDA/create_conda_env.sh
@@ -2,7 +2,6 @@ cd $1
 conda config --add channels r
 conda config --add channels bioconda
 
-conda env create --force --name CYTHON_FINDSV -f CYTHON.yml
 conda env create --force --name GENMOD_FINDSV -f GENMOD.yml
 conda env create --force --name samtools_FINDSV -f SAMTOOLS.yml
 conda env create --force --name VEP_FINDSV -f VEP_FINDSV.yml
@@ -10,5 +9,3 @@ conda env create --force --name VEP_FINDSV -f VEP_FINDSV.yml
 echo $2
 cd $2
 git clone https://github.com/J35P312/SVDB.git
-cd SVDB
-python setup.py build_ext --inplace
